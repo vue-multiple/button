012000061ac38383182acd2339d6f088389a20e6
savoygu <saifeizhoukou@163.com>
2017-07-24 16:47:29 +0800

    Add auto register support in browser

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ import Button from './components/button.vue'
 import ButtonGroup from './components/button-group.vue'
 import './styles/less/index.less'
 
+const install = function (Vue, opts = {}) {
+	if (install.installed) return
+	Vue.component('VmButton', Button)
+	Vue.component('VmButtonGroup', ButtonGroup)
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+	Vue.component('VmButton', Button)
+	Vue.component('VmButtonGroup', ButtonGroup)
+}
+
 export { Button, ButtonGroup }
